b92196e3c5c3ab0abbafe0ccaf6cdf1f1c4d1487
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-03 17:51:36 -0700

    client side more cleanup / setup for nav component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import '../../src/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1> hello </h1>
     </div>
   );
 }
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Hello from nav</h1>
+    </div>
+  );
+};
+
+export default Navbar;
